f8d60f1eef75f3f5f142f0c38eb8705584b835b0
IshikawaShinya <l.j.6.v.v.c@gmail.com>
2024-04-18 15:08:59 +0900

    ポートフォリオ画面の素材を配置

--- src/app/components/appBanner.tsx
+++ src/app/components/appBanner.tsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import { Dialog } from '@headlessui/react'
 import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/outline'
 import Link from 'next/link'
+import Image from 'next/image'
 
 
 const navigation = [
@@ -27,7 +28,7 @@ export default function appBanner(){
             }}
           />
         </div>
-        <div className="mx-auto max-w-2xl py-32 sm:py-48 lg:py-56">
+        <div aria-label='myName' className="mx-auto max-w-2xl py-32 sm:py-48 lg:py-56">
           <div className="hidden sm:mb-8 sm:flex sm:justify-center">
 
           </div>
@@ -41,20 +42,14 @@ export default function appBanner(){
               Next.js<br />
               Tailwindcss
             </p>
-            <div className="mt-10 flex items-center justify-center gap-x-6">
-              <a
-                href="#"
-                className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-              >
-                Get started
-              </a>
-              <a href="#" className="text-sm font-semibold leading-6 text-gray-900">
-                Learn more <span aria-hidden="true">→</span>
-              </a>
-            </div>
+          </div>
+          <div aria-label='fronend-engineer image'>
+            <Image src='/developer.jpg' width='50' height='50' alt={''}/>
           </div>
         </div>
+        <Image src='/signin_picture.png' width='100' height='100' alt={''}/>
         <div
+          aria-label='produdoImage'
           className="absolute inset-x-0 top-[calc(100%-13rem)] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[calc(100%-30rem)]"
           aria-hidden="true"
         >
