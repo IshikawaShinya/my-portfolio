dac0b11b6c1ff51238f9fb1fc96e78a527fa8018
IshikawaShinya <l.j.6.v.v.c@gmail.com>
2024-05-09 15:54:40 +0900

    TODO:gridに文章追加ができない。ちくしょう。

--- src/app/components/common/SkillGrid.tsx
+++ src/app/components/common/SkillGrid.tsx
@@ -3,24 +3,29 @@ import SkillCard from "../skills/Skill"
 type SkillInfo = {
     src: string,
     alt: string,
+    txt: string,
 }
 
 const skillInfos: SkillInfo[] = [
     {
         src: "/react_icon.png",
-        alt: "Transistor"
+        alt: "Transistor",
+        txt: "React",
     },
     {
         src: "/typescript.png",
         alt: "Reform",
+        txt: "TypeScript",
     },
     {
-        src: "/next.svg",
+        src: "/nextjs.svg",
         alt: "Tuple",
+        txt: "Nextjs",
     },
     {
         src: "https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600",
         alt: "SavvyCal",
+        txt: "Tailwindcss",
     },
 ]
 
@@ -31,9 +36,9 @@ export default function Skillgrid(){
                 <h2 className="text-center text-lg font-semibold leading-8 text-gray-900">
                     Skill Set
                 </h2>
-                <div className="mx-auto mt-10 grid max-w-lg grid-cols-4 items-center gap-x-8 gap-y-10 sm:items-center sm:max-w-4xl sm:grid-cols-4 sm:gap-x-5">
+                <div className="flex mx-auto mt-10 grid max-w-lg grid-cols-4 items-center gap-x-8 gap-y-10 sm:items-center sm:max-w-4xl sm:grid-cols-4 sm:gap-x-5">
                     {skillInfos.map((skillInfo, index) => (
-                        <SkillCard key={`${skillInfo.alt}-${index}`} src={skillInfo.src} alt={skillInfo.alt} />
+                        <SkillCard key={`${skillInfo.alt}-${index}`} src={skillInfo.src} alt={skillInfo.alt} txt={skillInfo.txt} />
                     ))}
                 </div>
             </div>
--- src/app/components/skills/Skill.tsx
+++ src/app/components/skills/Skill.tsx
@@ -1,17 +1,28 @@
 interface IProps {
     src: string
     alt: string
+    txt: string
 }
 
 export default function SkillCard(props: IProps) {
-    const { src, alt } = props;
+    const { src, alt, txt } = props;
     return (
+        // <div className="flex flex-col items-center col-span-2 max-h-12 w-full  sm:flex sm:flex-col sm:max-h-32 sm:items-center sm:col-span-1">
+        //     <img
+        //         className="object-cover h-full hover:-translate-y-1 duration-300 transition "
+        //         src={src}
+        //         alt={alt}
+        //         width={158}
+        //         height={48}
+        //     />
+        //     <p>{txt}</p>
+        // </div>
         <img
-            className="hover:-translate-y-1 duration-300 transition col-span-2 max-h-12 w-full object-contain lg:col-span-1"
-            src={src}
-            alt={alt}
-            width={158}
-            height={48}
+        className="hover:-translate-y-1 duration-300 transition col-span-2 max-h-12 w-full object-contain lg:col-span-1"
+        src={src}
+        alt={alt}
+        width={158}
+        height={48}
         />
     )
 }
