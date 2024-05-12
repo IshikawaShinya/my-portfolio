f41488410409fc84fb05eb9270a62f282656bd87
IshikawaShinya <l.j.6.v.v.c@gmail.com>
2024-05-12 18:46:59 +0900

    deployように整理

--- src/app/components/common/SkillGrid.tsx
+++ src/app/components/common/SkillGrid.tsx
@@ -31,7 +31,7 @@ const skillInfos: SkillInfo[] = [
 
 export default function Skillgrid(){
     return(
-        <div className="py-8">
+        <div className="pt-8 pb-24">
             <div className="mx-auto max-w-7xl px-6 lg:px-8">
                 <h2 className="text-center text-lg font-semibold leading-8 text-gray-900">
                     Skill Set
--- src/app/features/MyPage/page.tsx
+++ src/app/features/MyPage/page.tsx
@@ -3,8 +3,6 @@ import BackgroundImage from '@/app/components/common/BackgroundImage'
 import AppBanner from '@/app/components/common/AppBanner'
 import Header from '@/app/components/common/Header'
 import Skillgrid from '@/app/components/common/SkillGrid'
-import Product from '@/app/components/common/Product'
-import History from '@/app/components/common/History'
 
 const navigation = [
   { name: 'Product', href: '/features/product' },
@@ -21,8 +19,6 @@ export default function MyPage(){
       <BackgroundImage />
       <AppBanner />
       <Skillgrid />
-      <Product />
-      <History />
     </div>
   )
 }
